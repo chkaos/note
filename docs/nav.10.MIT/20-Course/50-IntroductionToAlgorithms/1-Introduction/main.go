25e9f0674b3fd746e7fa23f2699287cfcdce37ca
zhaojiongyang <zhaojiongyang@dataexa.com>
2020-10-12 19:50:05 +0800

    50 char1 compelte

--- docs/nav.10.MIT/20-Course/50-IntroductionToAlgorithms/1-Introduction/main.go
+++ docs/nav.10.MIT/20-Course/50-IntroductionToAlgorithms/1-Introduction/main.go
@@ -1,44 +1,9 @@
-## Peak Finding
+package main
 
-#### One dimensional peak finder
-可参考 leetcode 162. Find Peak Element, 输入时数字数组， 输出是“峰值”
+import (
+	"math"
+)
 
-~~~
-Input: nums = [1,2,1,3,5,6,4]
-Output: 1 or 5 
-Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
-~~~
-
-1. 最直接的暴力循环方法
-~~~go
-func findPeakElement(nums []int) int {
-    n:=len(nums)
-    // 只有一个元素不满足条件 返回0
-    if n==1{
-        return 0
-    }
-    // 数组大于1时，最后一个元素大于倒数第二个元素直接返回
-    if n>1&&nums[n-2]<nums[n-1]{
-        return n-1
-    }
-    // 暴力循环部分 复杂度为 O(n)
-    for i:=0;i<len(nums)-2;i++{
-        if i==0{
-            if nums[i+1]<nums[i]{
-                return i
-            }
-        }
-        if nums[i]<nums[i+1]&&nums[i+1]>nums[i+2]{
-            return i+1
-        }
-    }
-    return 0
-}
-~~~
-
-2. 二分法
-
-~~~go
 func binaryFindPeakElement(nums []int) int {
 	n := len(nums)
 	// 只有一个元素不满足条件 返回0
@@ -53,7 +18,7 @@ func binaryFindPeakElement(nums []int) int {
 	if n > 1 && nums[n-2] < nums[n-1] {
 		return nums[n-1]
 	}
-	// 递归二分查找 复杂度 O(lg n)
+	// 递归二分查找 复杂度 O(Logn)
 	return findPeakUtil(nums, 0, n-1)
 }
 
@@ -75,28 +40,17 @@ func findPeakUtil(arr []int, low, high int) int {
 		return findPeakUtil(arr, mid+1, high)
 	}
 }
-~~~
 
-#### 2D peak finder
-2D版本是1D的升级, 输入由单一数组变为矩阵(二维数组), "peak"定义的比较对象也从左右邻值拓展为上下左右.
+func findMax(matrix [][]int, row, mid int) (max, maxIndex int) {
+	for i := 0; i < row; i++ {
+		if matrix[i][mid] > max {
+			max = matrix[i][mid]
+			maxIndex = i
+		}
+	}
+	return
+}
 
-Examples:
-~~~
-Input : 10 20 15
-        21 30 14
-        7  16 32 
-Output : 30
-30 is a peak element because all its neighbors are smaller or equal to it. 32 can also be picked as a peak.
-
-Input : 10 7
-        11 17
-Output : 17
-~~~
-
-1. Greedy Ascent Algorithm 暴力破解(略, 复杂度为O(nm))
-
-2. 
-~~~go
 func twoDimensionPeakFinder(matrix [][]int) int {
 	rows := len(matrix)
 	if rows == 0 {
@@ -111,16 +65,6 @@ func twoDimensionPeakFinder(matrix [][]int) int {
 	mid := int(math.Floor(float64(columns / 2)))
 
 	return twoDimensionPeakHelper(matrix, rows, columns, mid)
-}
-
-func findMax(matrix [][]int, row, mid int) (max, maxIndex int) {
-	for i := 0; i < row; i++ {
-		if matrix[i][mid] > max {
-			max = matrix[i][mid]
-			maxIndex = i
-		}
-	}
-	return
 }
 
 func twoDimensionPeakHelper(matrix [][]int, rows, columns, mid int) int {
@@ -141,12 +85,7 @@ func twoDimensionPeakHelper(matrix [][]int, rows, columns, mid int) int {
 	// If max is less than its right
 	return twoDimensionPeakHelper(matrix, rows, columns, int(float64(mid)+math.Ceil(float64(mid)/2)))
 }
-~~~
-时间复杂度为O(nlogm)
 
-更多相关解法可参考[ProblemSet1]()
+func main() {
 
-#### 渐进复杂度
-渐进时间复杂度（asymptotic time complexity）就是当n趋于无穷大的时候，f(n) 得到的极限值。 可以理解为：我们通过计算得出一个算法的运行时间T(n), 与T(n)同数量级的即幂次最高的O(F(n))即为这个算法的时间复杂度。
-
-可以参考 https://goa.lenggirl.com/basic/dregee.html, 解释了几种渐进符号的含义.
+}
