ff22b12f0715d06a970666c9bb83802e4772b35f
chkaos <elviszhao2016@gmail.com>
2021-04-06 21:45:24 +0800

    修复一些不存在的图片链接

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,17 +28,17 @@ module.exports = {
     },
 
     plugins: ['permalink-pinyin', 'rpurl'],
-    // chainWebpack: (config, isServer) => {
-    //     const inlineLimit = 10000
-    //     config.module.rule('images')
-    //         .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
-    //         .use('url-loader')
-    //         .loader('url-loader')
-    //         .options({
-    //             limit: inlineLimit,
-    //             name: `assets/img/[name].[hash:8].[ext]`
-    //         })
-    // },
+    chainWebpack: (config, isServer) => {
+        const inlineLimit = 10000
+        config.module.rule('images')
+            .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
+            .use('url-loader')
+            .loader('url-loader')
+            .options({
+                limit: inlineLimit,
+                name: `assets/img/[name].[hash:8].[ext]`
+            })
+    },
     configureWebpack: {
         resolve: {
           alias: {
